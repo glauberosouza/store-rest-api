2fe79b5a0becc22d98a246d5eb8eb4e9a5367002
glauberosouza <glaubero.souza@gmail.com>
2024-10-18 21:54:50 -0300

    build

---
+++ src/main/java/com/store/demo/controller/ClientController.java
@@ -0,0 +1,15 @@
+package com.store.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+    @GetMapping
+    public String client(){
+        return "cliente";
+    }
+}
